1dedf98112af28ec20c2c0c43a910d8bd88063f8
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-01-27 12:53:28 +0100

    Corrected ParticleFlow

--- scripts/gppflow.jl
+++ scripts/gppflow.jl
@@ -67,8 +67,8 @@ fill_between!(xpred,μgp .- 2*sqrt.(siggp),μgp .+ 2*sqrt.(siggp),where=trues(le
 plot!(xpred,μgp,linewidth=3.0,color=:red)
 ## Training with Gaussian Noise
 M = 300
-# x_init = rand(MvNormal(xmap.minimizer,1.0),M)
-x_init = rand(MvNormal(zero(y_reg),1.0),M)
+x_init = rand(MvNormal(xmap.minimizer,1.0),M)
+# x_init = rand(MvNormal(zero(y_reg),1.0),M)
 x_t = copy(x_init)
 X_t = []
 x_p = Node(x_t)
@@ -77,11 +77,11 @@ mf = lift(x->x[1],m_and_sig)
 sigf = lift(x->sqrt.(max.(0.0,x[2])),m_and_sig)
 mfplus = lift(x->x[1].+sqrt.(max.(0.0,x[2])),m_and_sig)
 mfminus = lift(x->x[1].-sqrt.(max.(0.0,x[2])),m_and_sig)
-scene = scatter(sort(xrange),y_reg[xsort],markersize=0.1)
-plot!(xpred,mf,color=:blue,linewidth=2.0)
-fill_between!(xpred,mfminus,mfplus,color=:green)
+scene = Makie.scatter(sort(xrange),y_reg[xsort],markersize=0.1)
+Makie.plot!(xpred,mf,color=:blue,linewidth=2.0)
+Makie.fill_between!(xpred,mfminus,mfplus,color=:green)
 η=0.01
-opt_0 = [Flux.Descent(0.0001),Flux.Descent(0.0)]
+opt_0 = [Flux.Descent(0.01),Flux.Descent(0.01)]
 opt_x=[Flux.Descent(η),Flux.Descent(η)]
 # @progress for i in 1:100
 record(scene, plotsdir("gifs","gaussiangp.gif"), 1:100; framerate = 10) do i
@@ -116,10 +116,10 @@ xmap = optimize(p_stu,x0,LBFGS(),Optim.Options(iterations=1000,x_tol=1e-5,f_tol=
 @info "Optimization done"
 μgp, siggp = predict_f(m,xpred,covf=true)
 
-scatter(xrange[xsort],y_reg[xsort],markersize=0.1)
-plot!(sort(xrange),Optim.minimizer(xmap)[xsort],linewidth=3.0,color=:blue)
-fill_between!(xpred,μgp .- 2*sqrt.(max.(0.0,siggp)),μgp .+ 2*sqrt.(max.(0.0,siggp)),where=trues(length(xpred)),color=:red,alpha=0.2)
-plot!(xpred,μgp,linewidth=3.0,color=:red)
+Plots.scatter(xrange[xsort],y_reg[xsort],markersize=2.0)
+Plots.plot!(sort(xrange),Optim.minimizer(xmap)[xsort],linewidth=3.0,color=:blue)
+Plots.fill_between!(xpred,μgp .- 2*sqrt.(max.(0.0,siggp)),μgp .+ 2*sqrt.(max.(0.0,siggp)),where=trues(length(xpred)),color=:red,alpha=0.2)
+Plots.plot!(xpred,μgp,ribbon=2*sqrt.(max.(0.0,siggp)),linewidth=3.0,color=:red,fillalpha=0.2)
 ## Training with Student-T Noise
 M = 200
 
@@ -128,11 +128,17 @@ x_init = rand(MvNormal(xmap.minimizer,0.01),M)
 x_t = copy(x_init)
 X_t = []
 η=0.01
-opt_x=[Descent(η),Descent(η=η)]
-move_particles(x_t,p_stu,10,opt_x)
-μp,sigp = mu_and_sig(x_t)
+opt_x=[Descent(η),Descent(η)]
+move_particles(x_t,p_stu,opt_x)
+μp,sigp = m_and_C(x_t)
 sigp = diag(sigp)
-plot!(xrange[xsort],μp[xsort],ribbon=2*sqrt.(sigp),lw=3.0,lab="Particles")
+Plots.plot!(xrange[xsort],μp[xsort],ribbon=2*sqrt.(sigp),linewidth=3.0,color=:green)
+##
+μp,sigp = m_and_diagC(x_t)
+μf, σf = predic_f(p_reg,x_t,reshape(xpred,:,1))
+Plots.plot(xpred,μf,color=:green,linewidth=2.0)
+Plots.fill_between(xpred,μf.-sqrt.(max.(0.0,σf)),μf.+sqrt.(max.(0.0,σf)),color=:green,where=trues(length(xpred)))
+
 ################################################
 ## Optimizing for Classification
 p_log = GPModel((x,y)->sum(log.(avoid_inf.(logistic.(x.*y)))),kernel,variance,X,y_log,gradll=(x,p)->p.y.*logistic.(-p.y.*x))
@@ -142,17 +148,18 @@ m = VGP(collect(xrange),y_log,kernel,LogisticLikelihood(),AnalyticVI())
 train!(m,10)
 μgp,siggp = predict_f(m,collect(xrange)[xsort],covf=true)
 
-scatter(X,y_log,lab="data")
-plot!(xrange[xsort],Optim.minimizer(xmap)[xsort],lab="MAP",lw=3.0)
-plot!(xrange[xsort],μgp,ribbon=2*sqrt.(siggp),lab="MAP",lw=3.0,fillalpha=0.2)
-## Training with Student-T Noise
+Plots.scatter(vec(X),y_log,lab="data")
+Plots.plot!(xrange[xsort],Optim.minimizer(xmap)[xsort],lab="MAP",lw=3.0)
+Plots.plot!(xrange[xsort],μgp,ribbon=2*sqrt.(siggp),lab="GP",lw=3.0,fillalpha=0.2)
+## Training with Classification
 M = 200
 
 # x_init = rand(MvNormal(x_0.minimizer,0.01),M)
-x_init = rand(MvNormal(zero(y_reg),0.01),M)
+x_init = rand(MvNormal(zero(y_log),0.01),M)
 x_t = copy(x_init)
 X_t = []
-η=0.5
-opt_x=[VanillaGradDescent(η=η),VanillaGradDescent(η=η)]
-cb(x_t,p_log,0)
-move_particles(x_t,p_log,10,opt_x)
+η=0.01
+opt_x=[Descent(η),Descent(η)]
+for i in 1:10
+    move_particles(x_t,p_log,opt_x,cb=cb)
+end
