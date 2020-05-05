59af447091df8d3435024ad40535bda6840433b3
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-05 11:21:23 +0200

    Added scripts for AdvancedVI

---
+++ julia/avi/gp_advi_vs_stein_vs_pflow.jl
@@ -0,0 +1,186 @@
+using AdvancedVI; const AVI = AdvancedVI
+using Turing
+using LinearAlgebra
+using Distributions, DistributionsAD
+using Makie, StatsMakie, Colors, MakieLayout
+using KernelFunctions, Flux, KernelDensity
+
+N = 50
+x = range(0, 1, length = N)
+θ = log.([1.0, 10.0, 1e-3])
+k = exp(θ[1]) * transform(SEKernel(), exp(θ[2]))
+K = kernelmatrix(k, x) + 1e-5I
+f = rand(MvNormal(K))
+y = f + randn(N) * exp(θ[3])
+plot(x, y)
+##
+# xrange = range(min(mu_init,mu1)-3.0,max(mu_init,mu2)+3.0,length=300)
+
+# Makie.plot!(ax, x, f)
+# Makie.scatter!(ax, x, y)
+
+nParticles = 60
+max_iters = 2
+n_sig = 2
+
+setadbackend(:reverse_diff)
+function meta_logπ(θ)
+    k = exp(θ[1]) * transform(SEKernel(), exp(θ[2]))
+    K = kernelmatrix(k, x) + 1e-5I
+    d = TuringDenseMvNormal(zeros(length(x)), K)
+    return z -> sum(logpdf.(Normal.(y, exp(θ[3])), z)) + logpdf(d, z)
+end
+
+logπ_reduce = meta_logπ(θ)
+logπ_reduce(y)
+
+mu_init = randn(N)
+sig_init = exp.(randn(N))
+D = Matrix(Diagonal(sig_init))
+L_init = Matrix(cholesky(D).L)
+##
+# advi = Turing.Variational.ADVI(nParticles, max_iters)
+advi = AVI.ADVI(nParticles, max_iters)
+θ_init = vcat(mu_init, L_init[:])
+adq = AVI.transformed(TuringDenseMvNormal(mu_init, L_init*L_init'), AVI.Bijectors.Identity{1}())
+θ_ad = θ .- 1
+
+quadvi = AVI.ADQuadVI(nParticles, max_iters)
+# θ_init = vcat(mu_init, L_init[:])
+quadq = AVI.transformed(TuringDenseMvNormal(mu_init, L_init*L_init'), AVI.Bijectors.Identity{1}())
+
+steinvi = AVI.SteinVI(max_iters, transform(SqExponentialKernel(), 1.0))
+steinq =
+    AVI.SteinDistribution(rand(MvNormal(mu_init, sig_init), nParticles))
+θ_stein = θ .- 1
+
+gaussvi = AVI.PFlowVI(max_iters, false, true)
+gaussq = SamplesMvNormal(rand(MvNormal(mu_init, sig_init), nParticles))
+θ_gauss = θ .- 1
+
+# gaussq = AVI.transformed(SamplesMvNormal(rand(Normal(mu_init, sqrt(sig_init)),1,nParticles)),AVI.Bijectors.Identity{1}())
+
+α =  1.0
+optad = ADAGrad(α)
+optquad = ADAGrad(α)
+optstein = ADAGrad(α)
+optgauss = ADAGrad(α)
+hp_opt = ADAM(0.01)
+
+t = Node(0)
+meanad = lift(t) do t
+    adq.dist.m
+    # adqn = MvNormal((AVI.params(adq.dist) |> x -> (x[1],diag(x[2])))...)
+    # pdfad = pdf(Ref(adqn),xrange)
+end
+stdadplus = lift(t) do t
+    m, σ² = AVI.get_diag_params(adq.dist)
+    m + n_sig *sqrt.(σ²)
+end
+stdadminus = lift(t) do t
+    m, σ² = AVI.get_diag_params(adq.dist)
+    m - n_sig *sqrt.(σ²)
+end
+
+meanquad = lift(t) do t
+    quadq.dist.m
+end
+
+meanstein = lift(t) do t
+    mean(steinq)
+end
+
+meangauss = lift(t) do t
+    mean(gaussq)
+end
+stdgaussplus = lift(t) do t
+    mean(gaussq) + n_sig * sqrt.(diag(cov(gaussq)))
+end
+
+stdgaussminus = lift(t) do t
+    mean(gaussq) - n_sig * sqrt.(diag(cov(gaussq)))
+end
+
+scene, layout = layoutscene()
+ax = layout[1, 1] = LAxis(scene)
+
+ls = [Makie.plot!(ax, x, f, color=:red, linewidth=7.0),
+    Makie.plot!(ax, x, meanad, linewidth = 3.0, color = :green),
+    # Makie.plot!(ax, x, meanquad, linewidth = 3.0, color = :orange),
+    Makie.plot!(ax, x, meanstein, linewidth = 3.0, color = :blue),
+    Makie.plot!(ax, x, meangauss, linewidth = 3.0, color = :purple),
+    ]
+
+band!(ax, x, stdadplus, stdadminus, color = RGBA(colorant"green", 0.3), transparency =true)
+band!(ax, x, stdgaussplus, stdgaussminus, color = RGBA(colorant"purple", 0.3), transparency =true)
+# Makie.scatter!(ax, gaussxs, zeros(gaussq.n_particles), msize = 1.0)
+leg = layout[1,1] = LLegend(scene,ls,
+                    ["p(x)",
+                    "ADVI",
+                    # "Quad VI",
+                    "Stein VI",
+                    "Gaussian Particles",
+                    ],
+                    width=Auto(false),height=Auto(false),
+                    halign = :left, valign = :top, margin = (10,10,10,10))
+scene
+
+
+##
+record(scene,joinpath(@__DIR__,"path_particles_ad_vs_steinflow.gif"),framerate=25) do io
+    for i in 1:500
+        # global adq = AVI.vi(logπ_reduce, advi, adq, θ_init, optimizer = optad)
+        # global quadq = AVI.vi(logπ_base, quadvi, quadq, θ_init, optimizer = optquad)
+        # AVI.vi(logπ_reduce, steinvi, steinq, optimizer = optstein)
+        # AVI.vi(logπ_reduce, gaussvi, gaussq, optimizer = optgauss)
+        global adq = AVI.vi(
+            meta_logπ,
+            advi,
+            adq,
+            θ_init,
+            optimizer = optad,
+            hyperparams = θ_ad,
+            hp_optimizer = hp_opt,
+        )
+        AVI.vi(
+            meta_logπ,
+            steinvi,
+            steinq,
+            optimizer = optstein,
+            hyperparams = θ_stein,
+            hp_optimizer = hp_opt,
+        )
+        AVI.vi(
+            meta_logπ,
+            gaussvi,
+            gaussq,
+            optimizer = optgauss,
+            hyperparams = θ_gauss,
+            hp_optimizer = hp_opt,
+        )
+        @show exp.(θ_gauss)
+        @show exp.(θ_ad)
+        t[] = i
+        recordframe!(io)
+    end
+end
+##
+
+##
+# AVI.vi(
+#     meta_logπ,
+#     gaussvi,
+#     gaussq,
+#     optimizer = optgauss,
+#     hyperparams = θ_init,
+#     hp_optimizer = hp_opt,
+# )
+#
+# @profiler AVI.vi(
+#         meta_logπ,
+#         gaussvi,
+#         gaussq,
+#         optimizer = optgauss,
+#         hyperparams = θ_init,
+#         hp_optimizer = hp_opt,
+#     )
---
+++ julia/avi/test_BNN.jl
@@ -0,0 +1,133 @@
+# Based on https://turing.ml/dev/tutorials/3-bayesnn/
+
+using Turing, Flux, Plots, Random;
+using AdvancedVI, Bijectors
+
+## Create the data
+
+# Number of points to generate.
+N = 80
+M = round(Int, N / 4)
+Random.seed!(1234)
+
+# Generate artificial data.
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+xt1s = Array([[x1s[i] + 0.5; x2s[i] + 0.5] for i = 1:M])
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+append!(xt1s, Array([[x1s[i] - 5; x2s[i] - 5] for i = 1:M]))
+
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+xt0s = Array([[x1s[i] + 0.5; x2s[i] - 5] for i = 1:M])
+x1s = rand(M) * 4.5; x2s = rand(M) * 4.5;
+append!(xt0s, Array([[x1s[i] - 5; x2s[i] + 0.5] for i = 1:M]))
+
+# Store all the data for later.
+xs = [xt1s; xt0s]
+ts = [ones(2*M); zeros(2*M)]
+
+# Plot data points.
+function plot_data()
+    x1 = map(e -> e[1], xt1s)
+    y1 = map(e -> e[2], xt1s)
+    x2 = map(e -> e[1], xt0s)
+    y2 = map(e -> e[2], xt0s)
+
+    Plots.scatter(x1,y1, color="red", clim = (0,1))
+    Plots.scatter!(x2, y2, color="blue", clim = (0,1))
+end
+
+plot_data()
+
+## Create Neural Net parameters
+
+# Turn a vector into a set of weights and biases.
+function unpack(nn_params::AbstractVector)
+    W₁ = reshape(nn_params[1:6], 3, 2);
+    b₁ = reshape(nn_params[7:9], 3)
+
+    W₂ = reshape(nn_params[10:15], 2, 3);
+    b₂ = reshape(nn_params[16:17], 2)
+
+    Wₒ = reshape(nn_params[18:19], 1, 2);
+    bₒ = reshape(nn_params[20:20], 1)
+    return W₁, b₁, W₂, b₂, Wₒ, bₒ
+end
+
+# Construct a neural network using Flux and return a predicted value.
+function nn_forward(xs, nn_params::AbstractVector)
+    W₁, b₁, W₂, b₂, Wₒ, bₒ = unpack(nn_params)
+    nn = Chain(Dense(W₁, b₁, tanh),
+               Dense(W₂, b₂, tanh),
+               Dense(Wₒ, bₒ, σ))
+    return nn(xs)
+end;
+
+## Create model
+
+# Create a regularization term and a Gaussain prior variance term.
+alpha = 0.09
+sig = sqrt(1.0 / alpha)
+
+# Specify the probabalistic model.
+@model bayes_nn(xs, ts) = begin
+    # Create the weight and bias vector.
+    nn_params ~ MvNormal(zeros(20), sig .* ones(20))
+
+    # Calculate predictions for the inputs given the weights
+    # and biases in theta.
+    preds = nn_forward(xs, nn_params)
+
+    # Observe each prediction.
+    for i = 1:length(ts)
+        ts[i] ~ Bernoulli(preds[i])
+    end
+end;
+
+
+## Perform inference
+
+N_particles = 200
+n_iters = 10
+
+m = bayes_nn(hcat(xs...), ts)
+logπ = Turing.Variational.make_logjoint(m)
+q =  SamplesMvNormal(randn(20, N_particles)*0.0001)
+gvi = PFlowVI(n_iters, false, false)
+q isa SamplesMvNormal
+
+α = 0.001
+opt = ADAM(α)
+
+
+nn_forward(hcat(xs...), q.μ)
+x_range = collect(range(-6,stop=6,length=25))
+y_range = collect(range(-6,stop=6,length=25))
+
+anim = Animation()
+@progress for i in 1:200
+    AVI.vi(logπ, gvi, q, optimizer = opt)
+    Z = [nn_forward([x, y], q.μ)[1] for x=x_range, y=y_range]
+    p = Plots.contourf(x_range, y_range, Z, title="i = $(i*n_iters)",clims=(0,1))
+    Plots.scatter!(eachrow(hcat(xs...))..., zcolor = ts,lab="")
+    if i % 20 == 0
+        display(p)
+    end
+    frame(anim)
+end
+gif(anim, fps = 5)
+##
+gr()
+p1 = Plots.heatmap(q.Σ, yflip = true)
+Plots.plot!([0.5, 6.5, 6.5, 0.5, 0.5], [0.5, 0.5, 6.5, 6.5, 0.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([6.5, 9.5, 9.5, 6.5, 6.5], [6.5, 6.5, 9.5, 9.5, 6.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([0.5, 9.5, 9.5, 0.5, 0.5], [0.5, 0.5, 9.5, 9.5, 0.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([9.5, 15.5, 15.5, 9.5, 9.5], [9.5, 9.5, 15.5, 15.5, 9.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([9.5, 17.5, 17.5, 9.5, 9.5], [9.5, 9.5, 17.5, 17.5, 9.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([15.5, 17.5, 17.5, 15.5, 15.5], [15.5, 15.5, 17.5, 17.5, 15.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([17.5, 19.5, 19.5, 17.5, 17.5], [17.5, 17.5, 19.5, 19.5, 17.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([19.5, 20.5, 20.5, 19.5, 19.5], [19.5, 19.5, 20.5, 20.5, 19.5],lab="",color=:black, lw= 3.0)
+Plots.plot!([17.5, 20.5, 20.5, 17.5, 17.5], [17.5, 17.5, 20.5, 20.5, 17.5],lab="",color=:black, lw= 3.0)
+p2 = Plots.heatmap(reshape(q.μ, 1, :), colorbar= false)
+layout = @layout [a{0.1h}
+                    b{0.9h}]
+Plots.plot(p2, p1, layout=layout)
