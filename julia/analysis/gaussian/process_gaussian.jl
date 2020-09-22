2206b7c4eba212b40967aad59f454b47f4c9586f
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-09-22 17:49:19 +0200

    Correct processing for Gaussians

--- julia/analysis/gaussian/process_gaussian.jl
+++ julia/analysis/gaussian/process_gaussian.jl
@@ -3,10 +3,12 @@ using DrWatson
 include(projectdir("analysis", "post_process.jl"))
 
 
-## Load data and filter it
+## Load data
 all_res = collect_results(datadir("results", "gaussian"))
 
-res = @linq all_res |> where(:dim .== 10) |> where(:n_iters .== 1000) |> where(:n_particles .== 11) |> where(:n_runs .== 10)
+
+## Treat one convergence file
+res = @linq all_res |> where(:dim .== 5) |> where(:n_iters .== 5000) |> where(:n_particles .== 11) |> where(:n_runs .== 10)
 
 @assert nrow(res) == 1 "Number of rows is not unique or is empty"
 
@@ -27,3 +29,46 @@ for (i, alg) in enumerate(algs)
     @eval plot!(p_Σ, $(t_alg), $(v_alg), ribbon = sqrt.($(v_var_alg)), label = $(labels[alg]), color = colors[$i])
 end
 display(plot(p_μ, p_Σ))
+
+## Treating all results at once
+
+res = @linq all_res |> where(:n_iters .== 2000) |> where(:n_runs .== 10) |> where(:full_cov .== false)
+dims = Float64.(Vector(res.dim))
+s = sortperm(dims)
+## Plot combined results
+p_t = plot(title="Time vs dims", xlabel = "Dim", ylabel = "Time [s]")
+p_μ = plot(title="Mean error vs dims", xlabel = "Dim", ylabel =L"\|\mu -\mu_{true}\|")
+p_Σ = plot(title="Cov error vs dims", xlabel = "Dim", ylabel = L"\|\Sigma -\Sigma_{true}\|")
+for (i, alg) in enumerate(algs)
+    @info "Processing $(alg)"
+    ft_alg = Symbol("ft_", alg); ft_var_alg = Symbol("ft_var_", alg)
+    fm_alg = Symbol("fm_", alg); fm_var_alg = Symbol("fm_var_", alg)
+    fv_alg = Symbol("fv_", alg); fv_var_alg = Symbol("fv_var_", alg)
+    @eval begin
+        global $(ft_alg) = []; global $(ft_var_alg) = [];
+        global $(fm_alg) = []; global $(fm_var_alg) = [];
+        global $(fv_alg) = []; global $(fv_var_alg) = [];
+    end
+    t_alg = Symbol("t_", alg); t_var_alg = Symbol("t_var_", alg)
+    m_alg = Symbol("m_", alg); m_var_alg = Symbol("m_var_", alg)
+    v_alg = Symbol("v_", alg); v_var_alg = Symbol("v_var_", alg)
+    for j in 1:nrow(res)
+        truth = res.d_target[j]
+        @info "Row $j (dim = $(res.dim[j]))"
+        @eval $(t_alg), $(t_var_alg) = process_time(res.$(alg)[$j])
+        @eval $(m_alg), $(m_var_alg) = process_means(res.$(alg)[$j], $(truth.m))
+        @eval $(v_alg), $(v_var_alg) = process_fullcovs(res.$(alg)[$j], vec($(truth.C.L) * $(truth.C.U)))
+        @eval begin
+            push!($(ft_alg), last($(t_alg)))
+            push!($(ft_var_alg), last($(t_var_alg)))
+            push!($(fm_alg), last($(m_alg)))
+            push!($(fm_var_alg), last($(m_var_alg)))
+            push!($(fv_alg), last($(v_alg)))
+            push!($(fv_var_alg), last($(v_var_alg)))
+        end
+    end
+    @eval plot!(p_t, dims[s], $(ft_alg)[s], ribbon = sqrt.($(ft_var_alg)[s]), label = $(labels[alg]))
+    @eval plot!(p_μ, dims[s], $(fm_alg)[s], ribbon = sqrt.($(fm_var_alg)[s]), label = $(labels[alg]))
+    @eval plot!(p_Σ, dims[s], $(fv_alg)[s], ribbon = sqrt.($(fv_var_alg)[s]), label = $(labels[alg]))
+end
+plot(p_t, p_μ, p_Σ, legend = false)
