25bc05c416fcbdf61c2c4dd2d7c33c34e87aaf15
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-10-03 19:10:17 +0200

    Swag with multiple threads

--- julia/scripts/run_swag.jl
+++ julia/scripts/run_swag.jl
@@ -3,23 +3,24 @@ using DrWatson
 using Pkg; Pkg.update()
 
 using Distributed, CUDA
-# if length(workers()) != length(devices()) || first(workers()) == 1
-#     addprocs(length(devices()))
-# end
-# @everywhere using DrWatson
-# @everywhere @quickactivate
-# @everywhere using CUDA
-#
-# # Assign one GPU per worker
-# asyncmap((zip(workers(), devices()))) do (p, d)
-#     remotecall_wait(p) do
-#         @info "Worker $p uses $d"
-#         device!(d)
-#     end
-# end
+if length(workers()) != length(devices()) || first(workers()) == 1
+    addprocs(length(devices()))
+end
+@everywhere using DrWatson
+@everywhere @quickactivate
+@everywhere using CUDA
+
+# Assign one GPU per worker
+asyncmap((zip(workers(), devices()))) do (p, d)
+    remotecall_wait(p) do
+        @info "Worker $p uses $d"
+        # context()
+        device!(d)
+    end
+end
 
 include(srcdir("bnn", "swag.jl"))
-# @everywhere include(srcdir("bnn", "swag.jl"))
+@everywhere include(srcdir("bnn", "swag.jl"))
 
 exp_ps = Dict(
     :n_epoch => 1,
@@ -37,8 +38,8 @@ exp_ps = Dict(
 ps = dict_list(exp_ps)
 @info "Will now run $(dict_list_count(exp_ps)) simulations"
 
-# pmap(run_SWAG, ps)
-run_SWAG(ps[4])
+pmap(run_SWAG, ps)
+# run_SWAG(ps[4])
 
 # for (i, p) in enumerate(ps)
     # @info "Running dict $(i)/$(length(ps)) : $(savename(p))"
