2a35504dfe81f59a7554761855a348a5996832ea
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-10-19 19:37:11 +0200

    Valid run_gaussians

--- julia/scripts/run_gaussians.jl
+++ julia/scripts/run_gaussians.jl
@@ -4,23 +4,31 @@ using DrWatson;
 using Pkg; Pkg.update()
 include(srcdir("gaussian", "gaussian_target.jl"))
 
+
+# Uncomment following lines for using parallelism
+do_parallel = true
 # Use parallelism
-# using Distributed
-# nthreads = 6 # Number of threads to use
-# if nprocs() < nthreads
-#     addprocs(nthreads-nprocs()+1) # Add the threads as workers
-# end
+if do_parallel
+    using Distributed
+    nthreads = 6 # Number of threads to use
+    if nprocs() < nthreads
+        addprocs(nthreads-nprocs()+1) # Add the threads as workers
+    end
+end
 
 # Load all needed packages on every worker
-# @everywhere using DrWatson
-# @everywhere quickactivate(@__DIR__)
-# @everywhere include(srcdir("gaussian", "gaussian_target.jl"))
+if do_parallel
+    @everywhere using DrWatson
+    @everywhere quickactivate(@__DIR__)
+    @everywhere include(srcdir("gaussian", "gaussian_target.jl"))
+end
+
 # Create a list of parameters
 exp_ps = Dict(
     :n_iters => 3000, # Number of iterations to run
     :n_runs => 5, # Number of repeated runs
-    :dim => vcat(2:9), # Dimension of the target
-    :n_particles => 0,#, 10, 20, 50, 100], # Number of particles used, nothing will give dim + 1
+    :dim => vcat(2:9, 10:9:99, 100:100:500), # Dimension of the target
+    :n_particles => [0, 10, 20, 50, 100], # Number of particles used, nothing will give dim + 1
     :full_cov => true,# false], # If the covariance is identity or a full covariance with varying eigenvalues
     :gpf => true, # Run GaussParticle Flow
     :advi => true, # Run Black Box VI
@@ -35,4 +43,7 @@ exp_ps = Dict(
 ps = dict_list(exp_ps)
 @info "Will now run $(dict_list_count(exp_ps)) simulations"
 # run for each dict the simulation
-map(run_gaussian_target, ps)
+if do_parallel
+    pmap(run_gaussian_target, ps)
+else
+    map(run_gaussian_target, ps)
