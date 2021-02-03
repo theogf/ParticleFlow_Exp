1be35083f077470d899de2898299b9c424fc3369
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-02-03 20:26:21 +0100

    Splitted running files

--- scripts/run_gaussians_iblr.jl
+++ scripts/run_gaussians_iblr.jl
@@ -6,7 +6,7 @@ include(srcdir("gaussian", "gaussian_target.jl"))
 
 # Use parallelism
 using Distributed
-nthreads = 60 # Number of threads to use
+nthreads = 12# Number of threads to use
 nthreads = min(nthreads, Sys.CPU_THREADS - 2)
 if nprocs() < nthreads
     addprocs(nthreads - nprocs() + 1) # Add the threads as workers
@@ -23,25 +23,25 @@ exp_ps = Dict(
     :n_dim => 50, #[50, 100], #[5, 10, 20], #50, 100], # Dimension of the target
     :n_particles => 0,#, 10, 20, 50, 100], # Number of particles used, nothing will give dim + 1
     :cond => [1, 10, 100],
-    :gpf => true, # Run GaussParticle Flow
-    :gf => true, # Run Gauss Flow
-    :dsvi => true, # Run Doubly Stochastic VI
-    :fcs => true, # Run Factorized Structure Covariance
+    :gpf => !true, # Run GaussParticle Flow
+    :gf => !true, # Run Gauss Flow
+    :dsvi => !true, # Run Doubly Stochastic VI
+    :fcs => !true, # Run Factorized Structure Covariance
     :iblr => true, # Run i Bayesian Rule
-    :svgd => true, # Run linear SVGD
-    :natmu => [true, false], # Use preconditionning on b
+    :svgd => !true, # Run linear SVGD
+    :natmu => true, # Use preconditionning on b
     :seed => 42, # Seed for experiments
     :cb_val => nothing, # Callback values
     :eta => 0.01,
     :opt_det => :Descent,
     :opt_stoch => :Descent,# :RMSProp],
-    :comp_hess => :rep,
+    :comp_hess => [:hess, :rep],
     :overwrite => :true,
     :mode => :display,
 )
 ps = dict_list(exp_ps)
 @info "Will now run $(dict_list_count(exp_ps)) simulations"
 # run for each dict the simulation
-run_gaussian_target(ps[1])
+# run_gaussian_target(ps[1])
 # map(run_gaussian_target, ps)
-# pmap(run_gaussian_target, ps)
+pmap(run_gaussian_target, ps)
